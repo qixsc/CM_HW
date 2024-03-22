1eb655d6f350cca372641104d4f747df41d5885d
qixsc <qixiang9sc@gmail.com>
2024-03-22 13:39:03 +0000

    Mar 22, 2024, 9:39 PM

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -4,13 +4,13 @@ a = [1]
 b = [4]
 
 def f(x):
-    return x**3 + x + 4
+    return x**3 + x - 4
 
 while 1:
     mid = (a[-1] + b[-1])/2
     val = f(mid)
+
     print(mid, val)
-    os.system("pause")
 
     if abs(val) < 0.001:
         break
