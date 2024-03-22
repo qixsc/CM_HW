5ddd1e3b575c76ec2a2f8cbc2e322693280f25d7
qixsc <qixiang9sc@gmail.com>
2024-03-22 13:34:18 +0000

    Mar 22, 2024, 9:34 PM

---
+++ HW2/HW2.py
@@ -0,0 +1,16 @@
+a = [1]
+b = [4]
+
+def f(x):
+    return x**3 + x + 4
+
+while 1:
+    mid = (a[-1] + b[-1])/2
+    val = f(mid)
+    print(mid, val)
+    if abs(val) < 0.001:
+        break
+    elif val > 0:
+        b.append(mid)
+    else:
+        a.append(mid)
