ffb50df9b496bcce93393e00032455a6cd8c6dfa
qixsc <qixiang9sc@gmail.com>
2024-03-24 12:41:15 +0000

    Mar 24, 2024, 8:41 PM

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -38,9 +38,9 @@ def problem_3():
     for i in functions.keys():
         f = functions[i]
         p_now = 1
-        for _ in range(10):
+        for k in range(20):
             p_now = f(p_now)
-            print("err:", abs(p_now - p))
+            print(k+1, "err:", abs(p_now - p))
         try:
             alpha = math.log(abs((f(f(p_now)) - p)/(f(p_now) - p)))/math.log(abs((f(p_now) - p)/(p_now - p)))
             print(i, alpha)
