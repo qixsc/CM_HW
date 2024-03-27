520fbe5fca58cba8035453106f542bac9f262cd3
qixsc <qixiang9sc@gmail.com>
2024-03-27 13:12:07 +0000

    Mar 27, 2024, 9:12 PM

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -149,38 +149,32 @@ def problem_11():
 
 def problem_11_e():
     import typing
-    from cmath import sqrt  # Use the complex sqrt as we may generate complex numbers
+    from cmath import sqrt  
 
     Num = typing.Union[float, complex]
     Func = typing.Callable[[Num], Num]
 
-    def div_diff(f: Func, xs: list[Num]):
-        """Calculate the divided difference f[x0, x1, ...]."""
-        if len(xs) == 2:
-            a, b = xs
-            return (f(a) - f(b)) / (a - b)
-        else:
-            return (div_diff(f, xs[1:]) - div_diff(f, xs[0:-1])) / (xs[-1] - xs[0])
+    def diff(f: Func, x: list[Num]):
+        a, b = x
+        return (f(a) - f(b)) / (a - b)
 
-    def mullers_method(f: Func, xs: (Num, Num, Num), iterations: int) -> float:
-        """Return the root calculated using Muller's method."""
-        x0, x1, x2 = xs
+    def mullers_method(f: Func, x: (Num, Num, Num), iterations: int) -> float:
+        x_0, x_1, x_2 = x
         for _ in range(iterations):
-            w = div_diff(f, (x2, x1)) + div_diff(f, (x2, x0)) - div_diff(f, (x2, x1))
-            s_delta = sqrt(w ** 2 - 4 * f(x2) * div_diff(f, (x2, x1, x0)))
-            denoms = [w + s_delta, w - s_delta]
+            delta_1, delta_2 = diff(f, (x_1, x_0)), diff(f, (x_2, x_1))
+            d = (delta_2 - delta_1)/(x_2-x_0)
+            b = delta_2 + (x_2-x_1)*d
+            D = sqrt(b**2 - 4*f(x_2)*d)
+            E = max((b-D, b+D), key=abs)
             # Take the higher-magnitude denominator
-            x3 = x2 - 2 * f(x2) / max(denoms, key=abs)
+            x_3 = x_2 - 2 * f(x_2) / E
             # Advance
-            x0, x1, x2 = x1, x2, x3
+            x_0, x_1, x_2 = x_1, x_2, x_3
         return x3
 
-    def f_example(x: Num) -> Num:
-        """The example function. With a more expensive function, memoization of the last 4 points called may be useful."""
-        return x**3 - 5*x**2 + 8*x - 6
-
-    root = mullers_method(f_example, (0, 1, 2), 1)
-    print("Root: {}".format(root))  # Root: (24.738633317099097+0j)
+    f = lambda x : x**3 - 5*x**2 + 8*x - 6
+    m = mullers_method(f, (0, 1, 2), 1)
+    print(m)
 
 if __name__ == "__main__":
     problem_11_e()
