9952d29fbab24221a3832f55c4b86c82749403fa
qixsc <qixiang9sc@gmail.com>
2024-03-27 09:13:10 +0000

    Mar 27, 2024, 5:13 PM

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -116,7 +116,7 @@ def problem_10():
             p_1 = f(p_0)
             p_2 = f(p_1)
             p = p_0 - ((p_1 - p_0)**2)/(p_2 - 2*p_1 + p_0)
-            print(f"i: {i}, p_0: {p_0}, p_1: {p_1}, p_2: {p_2}, p:{p}")
+            print(f"{i} & {p_0} & {p_1} & {p_2} & {p}\\\\")
             x.append(p)
 
     from math import cos
